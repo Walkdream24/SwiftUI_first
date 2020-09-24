2fbdb8261642cb2fff2dece177a475d021b5d3a2
Walkdream24 <walkdream24@gmail.com>
2020-09-24 10:39:34 +0900

    Initial Commit

---
+++ SwiftUI_first/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_first
+//
+//  Created by Nakashige Ayumu on 2020/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_first/SwiftUI_firstApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_firstApp.swift
+//  SwiftUI_first
+//
+//  Created by Nakashige Ayumu on 2020/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_firstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
