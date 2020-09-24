f7e3c9654a2647a8b0ae49edb5763c411f1833e7
Walkdream24 <walkdream24@gmail.com>
2020-09-24 20:40:09 +0900

    apple_tutorial

--- SwiftUI_first/ContentView.swift
+++ SwiftUI_first/ContentView.swift
@@ -10,10 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
-            .padding()
+        
     }
 }
-//swiftUIfirst
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
